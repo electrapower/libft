9e5e3894683cf6afc75c2b067897983ab5eeefb4
electra <applehoob@gmail.com>
2024-11-24 21:02:30 +0100

    12345

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,6 +1,43 @@
 #include "libft.h"
 
-t_list  *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
+	t_list	*newnode;
 
+	newnode = (t_list *)malloc(sizeof(t_list));
+	if (!newnode)
+		return (NULL);
+	newnode->content = content;
+	newnode->next = NULL;
+	return (newnode);
 }
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char    *data;
+    t_list  *node;
+
+    data = "hello, list!";
+
+    // new node creating
+    node = ft_lstnew((void *)data);
+
+    // check success
+    if (!node)
+    {
+        printf("failed to allocate memory for new node\n");
+        return (1);
+    }
+
+    // print content
+    printf("node content: %s\n", (char *)node->content);
+
+    // free memory
+    free(node);
+
+    return (0);
+}
+*/
--- ft_split.c
+++ ft_split.c
@@ -74,7 +74,7 @@ char    **ft_split(char const *s, char c)
     splitted[i] = NULL;
     return (splitted);
 }
-/*
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -98,4 +98,4 @@ int main(void)
     free(result);
 
     return (0);
-}*/
+}
